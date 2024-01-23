d88debc39144f92f5c104dd8a7ecd458f8e110bc
moazzamsadiq <moazzam@syntaxtechs.com>
2024-01-22 21:17:39 -0500

    dfdsfS

---
+++ src/test/java/steps/test.java
@@ -0,0 +1,16 @@
+package steps;
+
+import java.io.IOException;
+
+import static Utils.CommonMethods.OpenBrowserAndLaunchApplication;
+
+public class test {
+
+    public static void main(String[] args) throws IOException {
+
+        OpenBrowserAndLaunchApplication();
+
+    }
+
+}
+//8:50 break
